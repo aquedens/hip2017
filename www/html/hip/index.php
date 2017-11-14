e11c89ddf128b0fd75a5a760dd142903ec954215
aquedens <31899382+aquedens@users.noreply.github.com>
2017-11-14 14:01:23 -0500

    Added www folder

---
+++ www/html/hip/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="The Controller For An RC Car Drone">
+        <meta name="author" content="Alex Quedens">
+        <link rel="icon" href="/var/www/html/imgs/icon.png">
+
+        <title>RC Car Drone Controller - Home</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        
+    </head>
+    <body alink="white" vlink="whtie">
+        
+        
+            <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">RC Camera Car</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Control</a>
+            </li>
+            <li class="nav-item mr-1">
+              <a class="nav-link" href="#">Details</a>
+            </li>
+          </ul>
+        
+        </div>
+      </nav>
+    </header>
+        
+        <div id="nav1">
+    <a href="index.html"><h1>RC Car Drone</h1></a>
+    <ul>
+        <li><a href=index.html>HOME</a></li>   
+        <li><a href=control.html>CONTROL</a></li>
+        <li><a href=details.html>DETAILS</a></li>
+    </ul>
+    </div>
+    <div id="main-content">
+    
+    <div id="page-name-header">    
+    <center>
+    <h1>Home</h1>
+    </center>
+        </div>
+    <div id="paragraph-welcome">
+        
+        <center><p>Welcome, this is a test.</p></center>
+        </div>
+        <div id="button-control">
+        <center><br><br><br><a href=control.html>Control the Drone!</a></center>
+        </div>
+    </div>
+            <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.js"></script>
+    </body>
+</html>
